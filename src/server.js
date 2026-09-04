3cf251121ab97d4fb6d466a07657b53bf47b71dd
Alessandro <mejiaalessandro2@gmail.com>
2026-09-03 22:27:21 -0700

    Correcion en la url en cors

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://fabulous-tapioca-363cb2.netlify.app/",
+      "https://fabulous-tapioca-363cb2.netlify.app",
     ],
   }),
 );
