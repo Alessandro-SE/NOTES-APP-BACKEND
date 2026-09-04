0fb29b5dd3284eebf36d7cd70b962eb162037404
Alessandro <mejiaalessandro2@gmail.com>
2026-09-03 22:22:29 -0700

    Agregamos a cors el origin permitido de nuestro front

--- src/server.js
+++ src/server.js
@@ -1,27 +1,30 @@
-import express from "express"
-import notesRouter from "./routes/notesRoutes.js"
-import dotenv from "dotenv"
-import { connectDB } from "./config/db.js"
-import cors from "cors"
-dotenv.config()
-const app = express()
+import express from "express";
+import notesRouter from "./routes/notesRoutes.js";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import cors from "cors";
+dotenv.config();
+const app = express();
 
-app.use(cors({
-    origin: "http://localhost:5173"
-}))
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://fabulous-tapioca-363cb2.netlify.app/",
+    ],
+  }),
+);
 
-app.use(express.json())
+app.use(express.json());
 
+app.use("/api/notes", notesRouter);
 
-app.use("/api/notes", notesRouter)
+const PORT = process.env.PORT || 3001;
 
-const PORT = process.env.PORT || 3001
+console.log(process.env.PORT);
 
-console.log(process.env.PORT)
-
-connectDB()
-.then(() => {
-    app.listen(PORT, () => {
-        console.log(`Servidor levantado en puerto http://localhost:${PORT}`)
-    })
-})
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Servidor levantado en puerto http://localhost:${PORT}`);
+  });
+});
