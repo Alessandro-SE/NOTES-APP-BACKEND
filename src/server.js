b84108038c741be576bfe678520dde1bc88e0e11
Alessandro <mejiaalessandro2@gmail.com>
2026-09-05 22:56:00 -0700

    probando server

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
+      "http://localhost:10000",
       "https://fabulous-tapioca-363cb2.netlify.app",
     ],
   }),
